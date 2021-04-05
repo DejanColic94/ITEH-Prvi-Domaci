8505e3ee4ecf58156d3574455f818009bb89b3a3
Dejan Colic <dejan.colic99@gmail.com>
2021-04-05 15:25:39 +0200

    initial commit, log in page

---
+++ logIn.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+<form class="box" action="" method="post">
+  <h1>Log In</h1>
+  <input type="text" name="username" placeholder="Enter Username">
+  <input type="password" name="password" placeholder="Enter Password">
+  <input type="submit" name="button" value="Log In">
+</form>
+
+
+  </body>
+</html>
